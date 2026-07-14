3e9e0ae482a51ca5fd6a2235a3bec3787d691725
negeevmaks0 <negeevmaks@gmail.com>
2026-07-14 11:21:10 +0200

    Init commit

--- psapp/serializers.py
+++ psapp/serializers.py
@@ -19,7 +19,7 @@ class UserSerializer(serializers.ModelSerializer):
 
 
 class RegisterSerializer(serializers.ModelSerializer):
-    password = serializers.CharField(write_only=True)
+    password = serializers.CharField(write_only=True, min_length=8)
 
 
     class Meta:
