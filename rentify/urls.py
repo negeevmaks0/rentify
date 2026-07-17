cd12fd571e8b7ae789b34efb461459a8e22e6d97
negeevmaks0 <negeevmaks@gmail.com>
2026-07-17 22:29:32 +0200

    feat(frontend): create base layout, navigation bar and global dark theme

--- rentify/urls.py
+++ rentify/urls.py
@@ -19,9 +19,11 @@ from django.urls import path, include
 
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic import TemplateView
 
 
 urlpatterns = [
+    path('', TemplateView.as_view(template_name='index.html'), name='home'),
     path('admin/', admin.site.urls),
     path('api/users/', include('users.urls')),
     path('api/properties/', include('properties.urls')),
