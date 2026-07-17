c3072c5810676e5a21ee31cb538466f7fd97ddf6
negeevmaks0 <negeevmaks@gmail.com>
2026-07-17 08:06:21 +0200

    Implement JWT authentication with HttpOnly cookies and auto login after registration

--- rentify/settings.py
+++ rentify/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 from pathlib import Path
 from environ import Env
 
+from datetime import timedelta
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -61,13 +63,20 @@ INSTALLED_APPS = [
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework_simplejwt.authentication.JWTAuthentication'
+        'users.authentication.CookieJWTAuthentication',
     ],
 
     'DEFAULT_FILTER_BACKENDS': [
         'django_filters.rest_framework.DjangoFilterBackend'
     ]
 }
+
+
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=30),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=7)
+}
+
 
 
 MIDDLEWARE = [
--- users/views.py
+++ users/views.py
@@ -1,4 +1,4 @@
-from rest_framework import generics
+from rest_framework import generics, status
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.generics import RetrieveUpdateAPIView
 from rest_framework_simplejwt.views import TokenObtainPairView
@@ -6,11 +6,54 @@ from rest_framework_simplejwt.views import TokenObtainPairView
 from .models import User
 from .serializers import UserSerializer, RegisterSerializer, LoginSerializer
 
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+from rest_framework_simplejwt.tokens import RefreshToken
+from django.contrib.auth import authenticate
+
 # Create your views here.
 
-class RegisterView(generics.CreateAPIView):
-    serializer_class = RegisterSerializer
-    queryset = User.objects.all()
+
+
+def set_token_cookies(response, user):
+    refresh = RefreshToken.for_user(user)
+
+    response.set_cookie(
+        'access_token',
+        str(refresh.access_token),
+        httponly=True,
+        samesite='Lax'
+    )
+
+    response.set_cookie(
+        'refresh_token',
+        str(refresh),
+        httponly=True,
+        samesite='Lax'
+    )
+
+    return response
+
+
+
+class RegisterView(APIView):
+    authentication_classes = []
+    permission_classes = []
+
+
+    def post(self, request):
+        serializer = RegisterSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+
+        user = serializer.save()
+
+        response = Response(
+            UserSerializer(user).data,
+            status=status.HTTP_201_CREATED
+        )
+
+        return set_token_cookies(response, user)
 
 
 
@@ -23,5 +66,37 @@ class ProfileView(RetrieveUpdateAPIView):
 
 
 
-class LoginView(TokenObtainPairView):
-    serializer_class = LoginSerializer
+class LogoutView(APIView):
+    def post(self, request):
+        response = Response({"detail": "Logged out"})
+
+        response.delete_cookie("access_token")
+        response.delete_cookie("refresh_token")
+
+        return response
+
+
+
+class LoginView(APIView):
+    authentication_classes = []
+    permission_classes = []
+
+
+    def post(self, request):
+        username = request.data.get("username")
+        password = request.data.get("password")
+
+        user = authenticate(username=username, password=password)
+
+        if not user:
+            return Response(
+                {"detail": "Invalid credentials"},
+                status=status.HTTP_401_UNAUTHORIZED
+            )
+
+        response = Response(
+            UserSerializer(user).data,
+            status=status.HTTP_200_OK
+        )
+
+        return set_token_cookies(response, user)
