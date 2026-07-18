efac1b9eaac376da9993e5f2c1c61b0077d7209a
negeevmaks0 <negeevmaks@gmail.com>
2026-07-18 22:59:08 +0200

    add property image upload from create page

--- static/js/property_create.js
+++ static/js/property_create.js
@@ -7,106 +7,130 @@ async function(e){
 e.preventDefault();
 
 
+
 const response = await fetch(
-"/api/properties/",
-{
-method:"POST",
+    "/api/properties/",
+    {
 
-credentials:"include",
+    method:"POST",
 
-headers:{
-"Content-Type":"application/json"
-},
+    credentials:"include",
+
+    headers:{
+        "Content-Type":"application/json"
+    },
 
 
-body:JSON.stringify({
+    body:JSON.stringify({
 
-title:
-document.getElementById("title").value,
+        title:
+            document.getElementById("title").value,
 
-description:
-document.getElementById("description").value,
+        description:
+            document.getElementById("description").value,
 
-location:
-document.getElementById("location").value,
+        location:
+            document.getElementById("location").value,
 
-price_per_month:
-document.getElementById("price").value,
+        price_per_month:
+            document.getElementById("price").value,
 
-room_count:
-document.getElementById("rooms").value,
+        room_count:
+            document.getElementById("rooms").value,
 
-property_type:
-document.getElementById("type").value
+        property_type:
+            document.getElementById("type").value
 
-})
+    })
 
 });
 
 
-if(response.ok){
+
+if(!response.ok){
+
+    const data = await response.json();
+
+    error.innerText =
+        JSON.stringify(data);
+
+    return;
+
+}
+
 
 
 const property = await response.json();
 
 
+
 const files =
-document.getElementById("images").files;
-
-
-for(
-let file of files
-){
-
-const formData = new FormData();
-
-
-formData.append(
-"property",
-property.id
-);
-
-
-formData.append(
-"image",
-file
-);
+    document
+    .getElementById("images")
+    .files;
 
 
 
-await fetch(
-"/api/properties/images/",
-{
+if(files.length > 0){
 
-method:"POST",
 
-credentials:"include",
+    const formData = new FormData();
 
-body:formData
+
+
+    for(
+        let i = 0;
+        i < files.length;
+        i++
+    ){
+
+        formData.append(
+            "images",
+            files[i]
+        );
+
+    }
+
+
+
+    const uploadResponse = await fetch(
+
+        `/api/properties/${property.id}/upload_image/`,
+
+        {
+
+        method:"POST",
+
+        credentials:"include",
+
+        body:formData
+
+        }
+
+    );
+
+
+
+    if(!uploadResponse.ok){
+
+        const data =
+            await uploadResponse.json();
+
+
+        error.innerText =
+            JSON.stringify(data);
+
+        return;
+
+    }
+
 
 }
 
-);
-
-
-}
 
 
 window.location="/properties/";
 
-}
-
-
-else{
-
-const data =
-await response.json();
-
-
-document.getElementById("error").innerText =
-JSON.stringify(data);
-
-}
 
 
 });
